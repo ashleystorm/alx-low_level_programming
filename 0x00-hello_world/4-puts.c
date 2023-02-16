d15a440c6e5962d6b5e58737f0398a6b7f4eb278
ashleystorm <ashleymundalamo25@gmail.com>
2023-02-16 06:37:45 -0800

    u

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 # include <stdio.h>
 
-int main()
+
+int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");   /*
-	return 0;						
-}								        * The puts() function is used to write a line or string 
-									* to the output
-									*/
-
+  puts("\"Programming is like building a multilingual puzzle");
+  return 0;
+}
